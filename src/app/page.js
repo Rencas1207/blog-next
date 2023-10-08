774b5e9f03c2ec7f2a352d6eb0c5d490355060e1
Rencas <rencasdag.12@gmail.com>
2023-10-07 23:53:36 -0500

    render blog

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
-import Image from 'next/image'
+import { allBlogs } from 'contentlayer/generated'
 
 export default function Home() {
+  console.log(allBlogs);
   return (
     <main className="flex flex-col items-center justify-center p-24">
       <h1>Hello World</h1>
